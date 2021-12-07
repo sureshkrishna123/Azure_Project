ab9df369c10e0f05ec13f7260d820ce62fe6c7f5
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-07 20:09:58 +0530

    Create face_sample.py

---
+++ face_sample.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Face Recognition(Powered by Azure)")
+
+
+st.header('Face Recognition:')
+
+st.text("Using Azure I build to **_detect, identify and analyse_ faces** in images.")
+st.text("Detect the objects in images")
+uploaded_file = st.file_uploader("Choose a file")
+image = Image.open(uploaded_file)
+
+st.image(image, caption='Uploaded image')
+
+select=st.selectbox("select what you want to find in the image" ,['Faces','Age & emotions ','objects'])
+
+button_translate=st.button('Click me',help='To give the image')
