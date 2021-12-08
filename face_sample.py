e9db08dc059f34b07d7855c670006b4ea387490e
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-08 20:02:08 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -13,7 +13,7 @@ from io import BytesIO
 # To install this module, run:
 # python -m pip install Pillow
 from PIL import Image, ImageDraw
-import FaceClient
+import azure.cognitiveservices
 #from msrest.authentication import CognitiveServicesCredentials
 #from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
 st.title("Face Recognition(Powered by Azure)")
