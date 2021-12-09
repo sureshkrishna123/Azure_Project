3400805159140205a9672d2b7b52afb6a615ab67
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-09 13:06:27 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -34,53 +34,41 @@ button_translate=st.button('Click me',help='To give the image')
 
 if button_translate and uploaded_file:
     
-    KEY = "ea8c44f876804e43ab35a26a09d59da5"
+    import requests
+from io import BytesIO
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
 
-    # This endpoint will be used in all examples in this quickstart.
-    ENDPOINT = "https://recognition-ai.cognitiveservices.azure.com/"
 
-    
-    face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-    
+def draw_face(img):
 
-    
+    subscription_key = 'ea8c44f876804e43ab35a26a09d59da5'  # Replace with a valid subscription key (keeping the quotes in place).
+    BASE_URL = "https://recognition-ai.cognitiveservices.azure.com/" + '/face/v1.0/detect'  # Replace with your regional Base URL
+    headers = {
+    # Request headers
+    'Content-Type': 'application/octet-stream',   # this should be the content type
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+    response = requests.post(BASE_URL,  headers=headers, data=img)
+    faces = response.json()
+    print(faces)
+    def getRectangle(faceDictionary):
+        rect = faceDictionary['faceRectangle']
+        left = rect['left']
+        top = rect['top']
+        bottom = left + rect['height']
+        right = top + rect['width']
+        return ((left, top), (bottom, right))
 
-    response_detected_faces = face_client.face.detect_with_stream(
-        image=image,
-        detection_model='detection_03',
-        recognition_model='recognition_04',
-        return_face_landmarks=True
-    )
+    output_image = Image.open(BytesIO(img))
+    #For each face returned use the face rectangle and draw a red box.
+    draw = ImageDraw.Draw(output_image)
+    for face in faces:
+        draw.rectangle(getRectangle(face), outline='red')
+    return output_image
+    image_data = open('tst.jpg', "rb").read()
 
-    if not response_detected_faces:
-        raise Exception('No face detected')
+    image = draw_face(image_data)
+    plt.imshow(image)
 
-    print('Number of people detected: {0}'.format(len(response_detected_faces)))
-
-    print(vars(response_detected_faces[0]))
-    print(vars(response_detected_faces[0].face_landmarks).keys())
-    print(response_detected_faces[0].face_landmarks.mouth_left)
-
-    img =Image.open(image)
-    draw = ImageDraw.Draw(img)
-
-    for face in response_detected_faces:
-        rect = face.face_rectangle
-        left = rect.left
-        top = rect.top
-        right = rect.width + left
-        bottom = rect.height + top
-        draw.rectangle(((left, top), (right, bottom)), outline='green', width=5)
-
-        # mark the noise tip
-        x = face.face_landmarks.nose_tip.x
-        y = face.face_landmarks.nose_tip.y
-        draw.rectangle(((x, y), (x, y)), outline='white', width=7)
-
-        # draw the bounding box around the mouth
-        mouth_left = face.face_landmarks.mouth_left.x, face.face_landmarks.mouth_left.y
-        mouth_right = face.face_landmarks.mouth_right.x, face.face_landmarks.mouth_right.y
-        lip_bottom = face.face_landmarks.under_lip_bottom.x, face.face_landmarks.under_lip_bottom.y
-        draw.rectangle((mouth_left, (mouth_right[0], lip_bottom[1])), outline='yellow', width=2)
-
-    st.image(img, caption='Output image')
+    st.image(image, caption='Output image')
