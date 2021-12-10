3311a4e23e92cc98f4006b0d35a4d5f02dd119c7
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-10 16:20:36 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -24,7 +24,8 @@ st.header('Face Recognition:')
 st.text("Using Azure I build to **_detect, identify and analyse_ faces** in images.")
 st.text("Detect the objects in images")
 uploaded_file = st.file_uploader("Choose a file",type=None)
-#image = Image.open(uploaded_file)
+
+image = Image.open(io.BytesIO(uploaded_file))
 
 st.image(uploaded_file, caption='Uploaded image')
 
