1c7a779f00655ebef80f8b6457548a8482434cdb
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-11 09:21:27 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -53,7 +53,13 @@ if button_translate and image_file :
         'Content-Type': 'application/octet-stream',  
         'Ocp-Apim-Subscription-Key': subscription_key,
         }
-        response = requests.post(BASE_URL,  headers=headers, data=image)
+        params = {
+            'detectionModel':'detection_01',
+            'recognitionModel':'recognition_04',
+            'returnFaceAttributes':['age', 'emotion']
+    
+         }
+        response = requests.post(BASE_URL, parms=prams, headers=headers, data=image)
         faces = response.json()
         print(faces)
         def getRectangle(faceDictionary):
