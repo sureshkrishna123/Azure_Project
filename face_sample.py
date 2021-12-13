e27fec6fc107892a4d57e0a423c1c4e4f1fdde9f
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-13 10:57:56 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -101,7 +101,7 @@ if app_mode =='Translator':
     st.markdown("Translator is a cloud-based machine translation service you can use to translate text in with a simple REST API call. The service uses modern neural machine translation technology and offers statistical machine translation technology. Custom Translator is an extension of Translator, which allows you to build neural translation systems. The customized translation system can be used to translate text with Translator or Microsoft Speech Services")
     
     st.markdown("For more documentation on language support in Azure:[click here](https://docs.microsoft.com/en-us/azure/cognitive-services/translator/language-support)")
-     st.markdown('---')
+    st.markdown('---')
 
     detect=st.text_input('Enter the text to detect (No language Restriction):')
 
