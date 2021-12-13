d509b45d7c02ee1f286c1001dbba007183bba992
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-13 10:48:28 +0530

    Update face_sample.py

--- face_sample.py
+++ face_sample.py
@@ -34,37 +34,31 @@ st.markdown(
 st.sidebar.title('Test the API powered by azure')
 st.sidebar.subheader('select API want to test')
 app_mode = st.sidebar.selectbox('Choose the App mode',
-['About App','Translator','Face recognization']
+['Face recognization','Translator']
 )
 
 
-st.title("Face Recognition(Powered by Azure)")
+if app_mode=='Face Recognuzation':
+  st.title("Face Recognition(Powered by Azure)")
+  st.header('Face Recognition:')
+  st.text("Using Azure I build to **_detect, identify and analyse_ faces** in images.")
+  st.text("Detect the objects in images")
+  
+  image_file =  st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+  if image_file is not None:
+    img = Image.open(image_file)
+    st.image(image_file,width=250,caption='Uploaded image')
+    byte_io = BytesIO()
+    img.save(byte_io, 'PNG')
+    image = byte_io.getvalue()
 
+  select=st.selectbox("select what you want to find in the image" ,['Faces','Age & emotions ','objects'])
 
-st.header('Face Recognition:')
+  button_translate=st.button('Click me',help='To give the image')
 
-st.text("Using Azure I build to **_detect, identify and analyse_ faces** in images.")
-st.text("Detect the objects in images")
+  if button_translate and image_file :
 
- 
-
-
-image_file =  st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
-if image_file is not None:
-  img = Image.open(image_file)
-  st.image(image_file,width=250,caption='Uploaded image')
-  byte_io = BytesIO()
-  img.save(byte_io, 'PNG')
-  image = byte_io.getvalue()
-
-select=st.selectbox("select what you want to find in the image" ,['Faces','Age & emotions ','objects'])
-
-button_translate=st.button('Click me',help='To give the image')
-
-if button_translate and image_file :
-
-   def draw_face(img):
+    def draw_face(img):
 
         subscription_key = 'ea8c44f876804e43ab35a26a09d59da5'  
         BASE_URL = "https://recognition-ai.cognitiveservices.azure.com/" + '/face/v1.0/detect'  
@@ -95,6 +89,6 @@ if button_translate and image_file :
         return output_image
    #image_data = open(image_file, "rb").read()
 
-   image = draw_face(img)
+    image = draw_face(img)
 
-   st.image(image, caption='Output image')
+    st.image(image, caption='Output image')
