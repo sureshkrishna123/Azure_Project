f809bd2832e989cf7cf8336acaddd038237a2f8a
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-14 12:34:06 +0530

    Update Azure_project.py

--- Azure_project.py
+++ Azure_project.py
@@ -17,6 +17,7 @@ from io import BytesIO
 from io import BytesIO
 from PIL import Image
 from PIL import ImageDraw
+import json
 
 st.set_page_config(layout="wide")
 st.sidebar.image('images/Azure_Image.png', width=300)
@@ -26,7 +27,7 @@ st.sidebar.markdown('Face Api,Translator Api')
 
 app_mode = st.sidebar.radio(
     "",
-    ("About Me","Face Recognization","Translator"),
+    ("About Me","Face Recognization","Object Detection","Translator"),
 )
 
 
@@ -66,7 +67,6 @@ if app_mode=='Face Recognization':
     img.save(byte_io, 'PNG')
     image = byte_io.getvalue()
 
-  select=st.selectbox("select what you want to find in the image" ,['Faces','Age & emotions ','objects'])
 
   button_translate=st.button('Click me',help='To give the image')
 
@@ -106,7 +106,50 @@ if app_mode=='Face Recognization':
     image = draw_face(img)
 
     st.image(image, caption='Output image')
+    
+    
+    
+if app_mode =='Object Detection':
+    
+    st.image(os.path.join('./images','object.jpg'),use_column_width=True )
+    st.markdown("<h1 style='text-align: center; color: skyblue; '> Object Recognition </h1>", unsafe_allow_html=True)
 
+    st.title("Object Recognition(Powered by Azure)")
+
+    st.text("Using Azure I build to ** Object detection , it identify and analyse the image. ** ")
+    st.text("Detect the objects in images")
+
+    url_file =  title = st.text_input('Paste image address URL')
+
+    button_translate=st.button('Click me',help='To give the image')
+
+    if button_translate and url_file :
+   
+        subscription_key = 'afac470736ce49ca8352ec7c83736fc7'
+        endpoint = 'https://objectdetection21.cognitiveservices.azure.com/'
+        
+            # Add your Computer Vision subscription key and endpoint to your environment variables.
+        analyze_url = endpoint + "vision/v3.1/analyze"
+        
+        # Set image_url to the URL of an image that you want to analyze.
+        headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+        params = {'visualFeatures': 'Categories,Description,Color'}
+        data = {'url': url_file}
+        response = requests.post(analyze_url, headers=headers,params=params, json=data)
+        response.raise_for_status()
+
+        # The 'analysis' object contains various fields that describe the image. The most
+        # relevant caption for the image is obtained from the 'description' property.
+        analysis = response.json()
+        print(json.dumps(response.json()))
+        image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+        response_image = requests.get(url_file)
+        
+        # Display the image and overlay it with the caption.
+        aux_im = Image.open(BytesIO(response_image.content))
+        st.image(aux_im, caption=image_caption)
+
+ 
 if app_mode =='Translator':
    
     st.image(os.path.join('./images','unnamed.png'),use_column_width=True )
