7f37078e181d4fbc775e2f0b0ce45fd230be8364
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-14 12:50:00 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -3,6 +3,7 @@
 
 pip install --upgrade azure-cognitiveservices-vision-face
 
+import cgitb 
 import asyncio
 import io
 import glob
@@ -15,7 +16,12 @@ from urllib.parse import urlparse
 from io import BytesIO
 # To install this module, run:
 # python -m pip install Pillow
-
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from PIL import Image, ImageDraw, ImageFont
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -31,8 +37,9 @@ image_name = os.path.basename(image_url)
 
 response_detected_faces = face_client.face.detect_with_url(
     image_url,
-    detection_model='detection_03',
-    recognition_model='recognition_04'
+    detection_model='detection_01',
+    recognition_model='recognition_04',
+    return_face_attributes=['age', 'emotion'],
 
 )
 print(response_detected_faces)
@@ -43,37 +50,13 @@ if not response_detected_faces:
 print('Number of people detected: {0}'.format(len(response_detected_faces)))
 
 response_image = requests.get(image_url)
-img = Image.open(io.BytesIO(response_image.content))
+img = Image.open(io.BytesIO(respone_image.content))
 draw = ImageDraw.Draw(img)
+
+
+
 
 for face in response_detected_faces:
-    rect = face.face_rectangle
-    left = rect.left
-    top = rect.top
-    right = rect.width + left
-    bottom = rect.height + top
-    draw.rectangle(((left, top), (right, bottom)), outline='green', width=5)
-img.show()
-img.save('test.jpg')
-
-##Guess a person's emotion & age
-
-
-image_url = 'http://www.historyplace.com/kennedy/president-family-portrait-closeup.jpg'
-
-response_detection = face_client.face.detect_with_url(
-    image_url,
-    detection_model='detection_01',
-    recognition_model='recognition_04',
-    return_face_attributes=['age', 'emotion'],
-)
-
-response_image = requests.get(image_url)
-img = Image.open(io.BytesIO(response_image.content))
-draw = ImageDraw.Draw(img)
-
-
-for face in response_detection:
     age = face.face_attributes.age
     emotion = face.face_attributes.emotion
     neutral = '{0:.0f}%'.format(emotion.neutral * 100)
@@ -93,44 +76,8 @@ for face in response_detection:
     draw.text((right + 4, top+70), 'Happy: ' + happiness, fill=(255, 255, 255))
     draw.text((right + 4, top+105), 'Sad: ' + sandness, fill=(255, 255, 255))
     draw.text((right + 4, top+140), 'Angry: ' + anger, fill=(255, 255, 255))
+    
 
-img.show()
-img.save('test.jpg')
+    
+plt.imshow(img)
 
-
-# Detect if a face shows up in other photos/images
-image='http://www.historyplace.com/kennedy/president-family-portrait-closeup.jpg'
-response_detected_faces = face_client.face.detect_with_url(
-    image,
-    detection_model='detection_03',
-    recognition_model='recognition_04',  
-)
-face_ids = [face.face_id for face in response_detected_faces]
-
-img_source = 'http://www.historyplace.com/kennedy/president-family-portrait-closeup.jpg'
-response_face_source = face_client.face.detect_with_url(
-    img_source,
-    detection_model='detection_03',
-    recognition_model='recognition_04'    
-)
-face_id_source = response_face_source[0].face_id
-
-matched_faces = face_client.face.find_similar(
-    face_id=face_id_source,
-    face_ids=face_ids
-)
-response_image = requests.get(image)
-img = Image.open(io.BytesIO(response_image.content))
-draw = ImageDraw.Draw(img)
-
-
-for matched_face in matched_faces:
-    for face in response_detected_faces:
-        if face.face_id == matched_face.face_id:
-            rect = face.face_rectangle
-            left = rect.left
-            top = rect.top
-            right = rect.width + left
-            bottom = rect.height + top
-            draw.rectangle(((left, top), (right, bottom)), outline='green', width=5)
-img.show()
