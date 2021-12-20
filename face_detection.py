fbc7f85ff74424d89b7f9de07cb390b0d15703e3
N.V.Suresh Krishna <62717096+sureshkrishna123@users.noreply.github.com>
2021-12-20 13:19:00 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -32,11 +32,10 @@ KEY = "ea8c44f876804e43ab35a26a09d59da5"
 ENDPOINT = "https://recognition-ai.cognitiveservices.azure.com/"
 
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-image_url = 'https://lh3.googleusercontent.com/pw/AM-JKLWihuQfkjmAu7AG5xRaoCnB9PI5VI6dZGcXhqVtIMk6L9swLa0M8WziWt0uVOTjTEY03WKKzAppzFt62mbpYT9C5x5n-gI0S8TmfOzUOg2X6CceLspAVqWSKyXN96r7C6YNHSGXUw7xm1wK2utNAAjNXg=w2249-h1686-no?authuser=0'
-image_name = os.path.basename(image_url)
+img_file = open(r'demo.jpg', 'rb')
 
-response_detected_faces = face_client.face.detect_with_url(
-    image_url,
+response_detected_faces = face_client.face.detect_with_stream(
+     image=img_file,
     detection_model='detection_01',
     recognition_model='recognition_04',
     return_face_attributes=['age', 'emotion'],
@@ -50,7 +49,7 @@ if not response_detected_faces:
 print('Number of people detected: {0}'.format(len(response_detected_faces)))
 
 response_image = requests.get(image_url)
-img = Image.open(io.BytesIO(respone_image.content))
+img = Image.open(img_file)
 draw = ImageDraw.Draw(img)
 
 
@@ -80,4 +79,5 @@ for face in response_detected_faces:
 
     
 plt.imshow(img)
+
 
